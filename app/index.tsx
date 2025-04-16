1dcd02716550a57eba094a1b9b9c0295703f65d8
Kamohelo Koali <kamohelokoali201@gmail.com>
2025-04-16 21:04:28 +0200

    initialize project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="h-screen w-full flex justify-center items-center"
+    >
+      <Text className=" text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
